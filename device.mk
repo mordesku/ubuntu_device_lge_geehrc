d3aa6ed3263ac6bdb311c5c146c986bcab69d726
Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>
2013-11-25 21:05:11 +0200

    Initialize wlan by forcing it via sysfs
    Change-Id: Ibb7b136afc97bb1a030d443f8664b0a4906488df
    Signed-off-by: Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>

--- device.mk
+++ device.mk
@@ -67,6 +67,9 @@ PRODUCT_COPY_FILES += \
 	device/lge/mako/media_profiles.xml:system/etc/media_profiles.xml \
 	device/lge/mako/media_codecs.xml:system/etc/media_codecs.xml
 
+PRODUCT_COPY_FILES += \
+	device/lge/mako/init_wlan.sh:system/etc/init_wlan.sh
+
 # Prebuilt kl and kcm keymaps
 PRODUCT_COPY_FILES += \
 	device/lge/mako/apq8064-tabla-snd-card_Button_Jack.kl:system/usr/keylayout/apq8064-tabla-snd-card_Button_Jack.kl \
